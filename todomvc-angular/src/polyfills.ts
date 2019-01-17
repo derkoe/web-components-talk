068a3499641cd30ee67a28cb3dcd171c6df72e90
Christian Köberl <christian.koeberl@porscheinformatik.at>
2019-01-17 15:11:42 +0100

    Current version, fixed polyfills for Angular

--- todomvc-angular/src/polyfills.ts
+++ todomvc-angular/src/polyfills.ts
@@ -83,4 +83,3 @@ import 'zone.js/dist/zone';  // Included with Angular CLI.
 /***************************************************************************************************
  * APPLICATION IMPORTS
  */
-import '@webcomponents/custom-elements/custom-elements.min';
